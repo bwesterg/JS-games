ee6351b03f325f182c36ac1162fd16b98db45ea3
bwesterg <benton.westergaard@gmail.com>
2024-02-16 01:16:45 -0700

    fixes modulo calc for refresh

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let playerState = 'roll';
+const dropdown = document.getElementById('animations');
+dropdown.addEventListener('change', function(e){
+    playerState = e.target.value;
+})
+
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
@@ -7,7 +13,7 @@ const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
-let playerState = '';
+
 
 // let frameX = 0;
 // let frameY = 2;
@@ -17,19 +23,43 @@ const spriteAnimations = [];
 const animationStates = [
     {
         name: 'idle',
-        frames: 7
+        frames: 7,
     },
     {
         name: 'jump',
-        frames: 7
+        frames: 7,
     },
     {
         name: 'fall',
-        frames: 7
+        frames: 7,
     },
     {
         name: 'run',
-        frames: 9
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'getHit',
+        frames: 4,
     }
 ];
 animationStates.forEach((state, index) => {
